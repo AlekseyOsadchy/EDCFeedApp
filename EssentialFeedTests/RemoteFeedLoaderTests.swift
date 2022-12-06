739560846ae4b0a6bd8f776b37823c7be4b205b0
Alexey Osadchy <osadchy.ak@gmail.com>
2022-12-06 23:29:25 +0200

    RemoteFeedLoader does not request upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Aleksey Osadchy on 06.12.2022.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+                
+        XCTAssertNil(client.requestedURL)
+    }
+}
