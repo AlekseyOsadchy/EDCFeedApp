e1de1e0a8fe61ee6c95b349fe6ae3a022c9484fb
Alexey Osadchy <osadchy.ak@gmail.com>
2022-12-06 22:55:53 +0200

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Aleksey Osadchy on 06.12.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
