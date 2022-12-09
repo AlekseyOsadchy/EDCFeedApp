4da9053ca8af3bbc64e43c2b637f0b9a7b3785f3
Alexey Osadchy <osadchy.ak@gmail.com>
2022-12-09 22:24:42 +0200

    Add more test assertions for more status code

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -49,15 +49,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
-
-        let (sut, client) = makeSUT()
-
-        var captureError: [RemoteFeedLoader.Error] = []
-        sut.load { captureError.append($0) }
         
-        client.complete(withStatusCode: 400)
-
-        XCTAssertEqual(captureError, [.invalidData])
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
+            
+            var captureError: [RemoteFeedLoader.Error] = []
+            sut.load { captureError.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            XCTAssertEqual(captureError, [.invalidData])
+        }
     }
 }
 
