451b4022445c3f47f2827c5df8e03cbd2727b9f4
Alexey Osadchy <osadchy.ak@gmail.com>
2022-12-11 18:06:13 +0200

    Extract test setup to a helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,21 +11,8 @@ import EssentialFeed
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGetFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
-        let exp = expectation(description: "Wait a load completion")
-        
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 10.0)
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expectation 8 items in the test account feed")
             
@@ -42,7 +29,28 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
 }
 
-extension EssentialFeedAPIEndToEndTests {
+private extension EssentialFeedAPIEndToEndTests {
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait a load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 10.0)
+        return receivedResult
+    }
+}
+
+private extension EssentialFeedAPIEndToEndTests {
     
     private func expectedItem(at index: Int) -> FeedItem {
         
