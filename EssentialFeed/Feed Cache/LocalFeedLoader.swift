883ffa7afd6caf69024df716b45d801d23623d8b
Alexey Osadchy <osadchy.ak@gmail.com>
2022-12-20 23:30:11 +0200

    Segment functionalities into extensions

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -26,6 +26,16 @@ public final class LocalFeedLoader {
         self.currentDate = currentDate
     }
     
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays , to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
+    }
+}
+ 
+extension LocalFeedLoader {
+    
     public func save(_ feed: [FeedImage], completion: @escaping SaveCompletion) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
@@ -36,6 +46,16 @@ public final class LocalFeedLoader {
             }
         }
     }
+    
+    private func cache(_ feed: [FeedImage], completion: @escaping SaveCompletion) {
+        store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
+
+extension LocalFeedLoader {
     
     public func load(completion: @escaping RetrieveCompletion) {
         store.retrieve() { [weak self] result in
@@ -53,6 +73,9 @@ public final class LocalFeedLoader {
             }
         }
     }
+}
+
+extension LocalFeedLoader {
     
     public func validateCache() {
         store.retrieve { [weak self] result in
@@ -69,20 +92,6 @@ public final class LocalFeedLoader {
                 break
             }
         }
-    }
-    
-    private func cache(_ feed: [FeedImage], completion: @escaping SaveCompletion) {
-        store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
-            guard self != nil else { return }
-            completion(error)
-        }
-    }
-    
-    func validate(_ timestamp: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays , to: timestamp) else {
-            return false
-        }
-        return currentDate() < maxCacheAge
     }
 }
 
