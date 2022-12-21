4547f424d5e1a2965ef29adff1bbd143a4390d36
Alexey Osadchy <osadchy.ak@gmail.com>
2022-12-21 23:57:40 +0200

    Extract SUT creation to helper factory method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -69,7 +69,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for cache completion")
         
         sut.retrieve { result in
@@ -87,7 +87,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for retrieve completion")
         
@@ -108,7 +108,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertToEmptyCache_deliversInsertedValues() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for cache completion")
         let feed = uniqueImageFeed().local
         let timestamp = Date()
@@ -130,5 +130,14 @@ final class CodableFeedStoreTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
+    }
+}
+
+// MARK: - Helper
+
+private extension CodableFeedStoreTests {
+    
+    func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
 }
